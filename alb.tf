ca131ca9019293109a8bff64b267daa4391ebf9c
CJ <99667477+CJ-Regan@users.noreply.github.com>
2022-03-14 15:06:34 +0000

    Create alb.tf

---
+++ alb.tf
@@ -0,0 +1,31 @@
+# Create LoadBalancer
+
+resource "aws_lb" "web-alb" {
+  name               = "web-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb-sg.id]
+  subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+
+
+}
+
+resource "aws_lb_target_group" "alb-target-group" {
+  name     = "alb-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.prod-vpc.id
+  }
+
+
+resource "aws_lb_listener" "alb-listener" {
+  load_balancer_arn = aws_lb.web-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb-target-group.arn
+  }
+}
